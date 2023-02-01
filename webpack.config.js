0fe89665de36e7390a08f2d048ed4f9ec8d272d0
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-01 10:23:31 +0300

    Add rules for babel and css/style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,22 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ],
+  },
 };
