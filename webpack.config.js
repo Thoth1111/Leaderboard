cbbe0cf4942334e8b9c847da6cda5f08e3d45475
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-01 11:13:28 +0300

    Install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
@@ -14,6 +17,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+//   optimization: {
+//     runtimeChunk: 'single',
+//   },
   module: {
     rules: [
       {
