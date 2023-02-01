30b5b3fc85ac8f0a252dc21f42975632f932fe58
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-01 11:17:45 +0300

    Add modules dir and page.js module

---
+++ src/modules/page.js
@@ -0,0 +1,25 @@
+const content = document.getElementById('content');
+
+const head = `<h1>Leaderboard</h1>`
+
+const page = 
+`<section class="column">        
+    <div>
+        <h2>Recent Scores</h2>
+        <button type="button">Refresh</button>
+        <div class="scores-display"></div>
+    </div>
+    <div class="score-form">
+        <h2>Add your score</h2>
+        <input type="text" required placeholder="Your name"/>
+        <input type="number" required placeholder="Your score"/>
+        <button type="submit">Submit</button>
+    </div>
+</section>
+`
+const renderPage = () => {
+    content.insertAdjacentHTML('beforeend', head);
+    content.insertAdjacentHTML('beforeend', page);
+};
+
+export default renderPage
