887877ccca0288d2928c1a68b8dd060ba77ab900
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-03 08:20:17 +0300

    fix linter errors and run build

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import './style.css';
 import renderPage from './modules/page.js';
-import { displayScores } from './modules/pagefunctions.js';
+import displayScores from './modules/pagefunctions.js';
 import { saveScore } from './modules/LeaderApi.js';
 
 window.onload = () => {
-    renderPage();
+  renderPage();
+  displayScores();
+  const refreshBtn = document.querySelector('#refresher');
+  const inputArea = document.querySelector('#input-area');
+
+  refreshBtn.onclick = () => {
     displayScores();
-    const refreshBtn = document.querySelector('#refresher');
-    const inputArea = document.querySelector('#input-area');
+  };
 
-    refreshBtn.onclick = () => {
-        displayScores();
-    }
-
-    inputArea.onsubmit = (e) => {
-        e.preventDefault();
-        const player = document.querySelector('#player-id');
-        const score = document.querySelector('#score-input');
-        saveScore(player, score);
-        inputArea.reset();
-        displayScores();
-    }
-}
+  inputArea.onsubmit = (e) => {
+    e.preventDefault();
+    const player = document.querySelector('#player-id');
+    const score = document.querySelector('#score-input');
+    saveScore(player, score);
+    inputArea.reset();
+    displayScores();
+  };
+};
